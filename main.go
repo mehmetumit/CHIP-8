611538fcecef473730159cb45af43d31f6b89b03
Mehmet Ümit Özden <ozdenmehmetumit@gmail.com>
2023-05-01 14:58:36 +0300

    Command line args added

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"flag"
 	"github.com/mehmetumit/CHIP-8/chip8"
 )
 
 func main() {
-	chip8.Boot("./roms/Pong.ch8", 12, 3)
+	var romPath string
+	var displayScale int32
+	var speed uint8
+	flag.StringVar(&romPath, "path", "./roms/Instruction-Test.ch8", "The file path of rom")
+	speed = uint8(*flag.Uint("speed", 3, "The emulation speed"))
+	displayScale = int32(*flag.Int("scale", 12, "The display scale"))
+	flag.Parse()
+	chip8.Boot(romPath, displayScale, speed)
 }
